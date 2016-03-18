425baca935ca4bdb5faedfc9c3970308ff1420e8
Manuel Felipe S.R <manuelfelip-96@hotmail.com>
2016-03-18 00:23:37 -0500

    Implementado OK hasta .III

---
+++ Desktop/MyBatis-Intro/src/main/java/edu/eci/pdsw/samples/textview/MyBatisExample.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2015 hcadavid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package edu.eci.pdsw.samples.textview;
+
+
+import edu.eci.pdsw.samples.entities.Consulta;
+import edu.eci.pdsw.samples.entities.Paciente;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import edu.eci.pdsw.samples.mybatis.mappers.PacienteMapper;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class MyBatisExample {
+
+    /**
+     * Método que construye una fábrica de sesiones de MyBatis a partir del
+     * archivo de configuración ubicado en src/main/resources
+     *
+     * @return instancia de SQLSessionFactory
+     */
+    public static SqlSessionFactory getSqlSessionFactory() {
+        SqlSessionFactory sqlSessionFactory = null;
+        if (sqlSessionFactory == null) {
+            InputStream inputStream;
+            try {
+                inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException(e.getCause());
+            }
+        }
+        return sqlSessionFactory;
+    }
+
+    /**
+     * Programa principal de ejempo de uso de MyBATIS
+     * @param args
+     * @throws SQLException 
+     */
+    public static void main(String args[]) throws SQLException {
+        SqlSessionFactory sessionfact = getSqlSessionFactory();
+
+        SqlSession sqlss = sessionfact.openSession();
+
+        PacienteMapper pmap=sqlss.getMapper(PacienteMapper.class);
+
+        
+       //System.out.println(pmap.loadPacienteById(2,"cc"));
+        MyBatisExample exam = new MyBatisExample();
+        //Aca se agrega un paciente coon 0 consultas.
+    //  Paciente tmp = new Paciente(104, "CC", "Victor Elias", new java.sql.Date(0));
+      Consulta c = new Consulta(new java.sql.Date(0),"Nada");
+      //La consulta c es agregada  con su foranea como el id del Paciente tmp
+      exam.registrarConsultaaPaciente(pmap, c, 104, "CC");
+
+      
+      //Aca se agrega el paciente 
+        //exam.registrarNuevoPaciente(pmap,tmp);
+        
+        
+       //Cargamos el paciente para verificar sus consultas
+        System.out.println("Paciente cargado :" + pmap.loadPacienteById(104,"CC"));
+       sqlss.commit();
+        
+        
+        sqlss.close();
+
+    }
+
+    /**
+     * Registra un nuevo paciente y sus respectivas consultas (si existiesen).
+     * @param pmap mapper a traves del cual se hará la operacion
+     * @param p paciente a ser registrado
+     */
+    public void registrarNuevoPaciente(PacienteMapper pmap, Paciente p){
+    
+
+
+        pmap.insertPaciente(p);
+        System.out.println("Paciente ");
+         
+        
+    }
+    public void registrarConsultaaPaciente(PacienteMapper pmap,Consulta con,int id,String tipoid){
+        pmap.insertConsulta(con, id, tipoid);
+    }
+    
+
+}
